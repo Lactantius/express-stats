dcccfc3c72555572437cc81e30ca1e0ffc5bfab0
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-23 09:43:18 -0500

    Returns query string as number array

--- index.ts
+++ index.ts
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
+function parseNumString(numString: string): number[] {
+  return numString.split(",").map((num) => Number(num));
+}
+
+app.use(express.json());
+
+app.get("/mean", (req, res) => {
+  const nums = parseNumString(req.query.nums as string);
+  console.log(nums);
+  return res.json(nums);
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
