e165bdbc6c39d3bf2492fd5e9b53b3ca2a8cbc27
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-23 09:55:11 -0500

    Separate response formatting to separate function

--- index.ts
+++ index.ts
@@ -6,12 +6,23 @@ function parseNumString(numString: string): number[] {
   return numString.split(",").map((num) => Number(num));
 }
 
+interface ResponseJSON {
+  response: {
+    operation: string;
+    value: number;
+  };
+}
+
+function formatResponse(operation: string, value: number): ResponseJSON {
+  return { response: { operation: operation, value: value } };
+}
+
 app.use(express.json());
 
 app.get("/mean", (req, res) => {
   const nums = parseNumString(req.query.nums as string);
   const mean = nums.reduce((sum, n) => sum + n) / nums.length;
-  return res.json({ response: { operation: "mean", value: mean } });
+  return res.json(formatResponse("mean", mean));
 });
 
 app.listen(3000, () => {
