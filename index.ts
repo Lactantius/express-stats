e8ec3e23970550541384db65f7541bc56d0e84e6
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-23 10:39:29 -0500

    Messy mode function

--- index.ts
+++ index.ts
@@ -17,12 +17,36 @@ function formatResponse(operation: string, value: number): ResponseJSON {
   return { response: { operation: operation, value: value } };
 }
 
+interface Frequency {
+  [n: number]: number;
+}
+
+/*
+ * There is probably a better way to do this
+ */
+function getMode(nums: number[]): number {
+  const frequency = nums.reduce((obj, n) => {
+    obj[n] ? (obj[n] = obj[n] + 1) : (obj[n] = 1);
+    return obj;
+  }, <Frequency>{});
+  return Object.entries(frequency).reduce(
+    (highest, next) => (next[1] > highest[1] ? next : highest),
+    [0, 0]
+  )[0];
+}
+
 app.use(express.json());
 
 app.get("/mean", (req, res) => {
   const nums = parseNumString(req.query.nums as string);
   const mean = nums.reduce((sum, n) => sum + n) / nums.length;
   return res.json(formatResponse("mean", mean));
+});
+
+app.get("/mode", (req, res) => {
+  const nums = parseNumString(req.query.nums as string);
+  const mode = getMode(nums);
+  return res.json(formatResponse("mode", mode));
 });
 
 app.listen(3000, () => {
